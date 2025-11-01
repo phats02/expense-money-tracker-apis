96e39f63e680f9ae66a1e370b47d6152fb7f871c
phats02 <phattran.2002.work@gmail.com>
2025-11-01 20:08:24 +0700

    initial commit

---
+++ src/main/java/com/example/expenseMoneyTracker/ExpenseMoneyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenseMoneyTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMoneyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseMoneyTrackerApplication.class, args);
+	}
+
+}
