be33fc161e9909efe8309e2f0687660588ed3209
phats02 <phattran.2002.work@gmail.com>
2025-11-01 20:39:43 +0700

    feat: Add swagger

--- src/main/java/com/example/expenseMoneyTracker/ExpenseMoneyTrackerApplication.java
+++ src/main/java/com/example/expenseMoneyTracker/ExpenseMoneyTrackerApplication.java
@@ -1,13 +1,16 @@
 package com.example.expenseMoneyTracker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ExpenseMoneyTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseMoneyTrackerApplication.class, args);
+        log.info("🚀 App successfully started. Apis docs: http://localhost:8080/swagger-ui/index.html");
 	}
 
 }
--- src/main/java/com/example/expenseMoneyTracker/controller/HealthController.java
+++ src/main/java/com/example/expenseMoneyTracker/controller/HealthController.java
@@ -1,11 +1,13 @@
 package com.example.expenseMoneyTracker.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name="Check health of service")
 public class HealthController {
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
